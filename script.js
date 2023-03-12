31e95dbd911f436b252c9acee38d01ec494a3ccd
Cosmo <johan.amador200@gmail.com>
2023-03-11 23:30:50 -0500

    Eventos en JavaScript: interactuando con usuarios

--- script.js
+++ script.js
@@ -1,35 +1,11 @@
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafito = document.getElementsByClassName('parrafito');
-const pid = document.getElementById('pid');
-const input = document.querySelector('input');
+const input1 = document.getElementById('calculo1');
+const input2 = document.getElementById('calculo2');
+const btn = document.getElementById('btnCalcular');
+const pResult = document.querySelector('#result');
 
-// console.log(input.value);
 
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input,
-});
-
-h1.innerHTML = "Dux<br>Gang";   // Lo convierte a código HTML
-h1.innerText = "Dux<br>Gang";   // Lo convierte a texto normal
-// console.log(h1.getAttribute('class'));
-// h1.setAttribute('class', 'rojo');
-
-h1.classList.add('rojo');
-h1.classList.remove('verde');
-// h1.classList.toggle('rojo');
-// console.log(h1.classList.contains('rojo'));
-
-input.value = "456";
-
-const img  = document.createElement('img');
-img.setAttribute('src','https://avatars.githubusercontent.com/u/99379160?s=48&v=4');
-console.log(img);
-
-pid.innerHTML = null;
-pid.append(img);
-// pid.appendChild(img);
+function btnOnClick() {
+    const suma = Number(input1.value) + Number(input2.value);
+    pResult.innerHTML = "Resultado: " + suma;
+}
