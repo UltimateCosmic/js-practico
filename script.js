2ae6d9b13faa1585040fa7f9edf498e2cfd6895a
Cosmo <johan.amador200@gmail.com>
2023-03-15 02:54:51 -0500

    addEventListener

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
 const input1 = document.getElementById('calculo1');
 const input2 = document.getElementById('calculo2');
 const btn = document.getElementById('btnCalcular');
 const pResult = document.querySelector('#result');
 
+form.addEventListener('submit', sumarInputValues);
 
-function btnOnClick() {
+function sumarInputValues(event) {
+    // console.log({event});
+    event.preventDefault();
     const suma = Number(input1.value) + Number(input2.value);
     pResult.innerHTML = "Resultado: " + suma;
 }
